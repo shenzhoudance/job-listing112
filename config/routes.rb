4e9bea34e84d4216d4f5fa45f38f96aab35de0c3
xiaowei <shenzhoudance@gmail.com>
2017-07-13 09:50:22 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ root 'jobs#index'
 resources :jobs
 
 namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+       post :publish
+       post :hide
+     end
+    end
   end
   
 end
